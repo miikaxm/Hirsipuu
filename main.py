187d9aa3f7d5b11867813f944072aa72c478c56e
Miika Valkonen <miika.valkonen08@gmail.com>
2025-03-21 13:56:10 +0200

    koodit yhteen

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+import random
+
+class Sana:
+    def __init__(self, tiedoston_nimi = "sanakirja.txt"):
+        self.sanat = self.lataa_sanat(tiedoston_nimi)
+        
+    def lataa_sanat(self, tiedoston_nimi):
+        try:
+            with open(tiedoston_nimi) as tiedosto:
+                return [rivi.strip() for rivi in tiedosto.readlines() if rivi.strip()]
+        except FileNotFoundError:
+            print(f"Virhe: Tiedostoa {tiedoston_nimi} ei löytynyt. Käytetään oletussanoja.")
+            return["omena", "banaani", "kissa", "koira", "puhelin", "tietokone", "auto", "moottoripyörä"]
+        
+    def valitse_sana(self):
+        return random.choice(self.sanat)
+
 class Peli:
     def __init__(self):
-        self.sana = "koira"
+        self.sana = Sana().valitse_sana()
         self.arvattu = []
         self.max_vaarin = 6
         self.vaarat = 0
@@ -69,8 +86,10 @@ class Peli:
     def tarkista_arvaus(self, arvaus):
         if arvaus.lower() in self.sana:
             self.oikeat_kirjainmet.add(arvaus.lower())
+            self.arvattu.append(arvaus)
         else:
             self.vaarat += 1
+            self.arvattu.append(arvaus)
 
     # Tarkistaa onko peli ohi
     def peli_ohi(self):
@@ -90,6 +109,12 @@ class Peli:
             print(self.hirsipuu_kuvat[self.vaarat])
             print(self.näytettävä_sana())
             arvaus = input("Arvaa kirjain: ")
+            if not arvaus.isalpha():
+                print("Anna vain kirjaimia!")
+                continue
+            if arvaus in self.arvattu:
+                print("Olet jo arvannut tämän kirjaimen!")
+                continue
             if len(arvaus) != 1:
                 print("Anna tasan yksi kirjain!")
                 continue
